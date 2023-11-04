42bbde5c57a98e5e7c14da48acb69efe88f2dfcb
anthogonyst <anthogonyst@gmail.com>
2023-11-04 03:12:58 -0400

    Add file aggregation functions

---
+++ R/data-brooklyn-helpers.R
@@ -0,0 +1,50 @@
+
+library(magrittr)
+library(readr)
+library(here)
+
+.ReadCsv <- function(x, ...) {
+  readr::read_csv(x, show_col_types = FALSE, name_repair = "minimal", progress = FALSE, ...)
+}
+
+.ReadXls <- function(x, ...) {
+  readxl::read_excel(x, progress = FALSE, .name_repair = "minimal", ...)
+}
+
+.FixFirstColname <- function(df, name) {
+  colnames(df)[1] = name
+  df
+}
+
+LoadBrooklynSalesMap <- function() {
+  files = list.files(here::here("data"), "brooklyn_sales_map.*Part\\d\\.csv", full.names = T) %>%
+    c(., list.files(here::here("data"), "brooklyn_sales_map.*Part\\d\\d\\.csv", full.names = T))
+  
+  indexCol = "IndexColX"
+  
+  lapply(files, .ReadCsv) %>%
+    lapply(., .FixFirstColname, indexCol) %>%
+      do.call(cbind, .) %>%
+        .[, ! grepl(indexCol, colnames(.))]
+}
+
+LoadDoF <- function() {
+  files = list.files(here::here("data/dof-rolling-sales/"), "brooklyn", full.names = T)
+  
+  years = files %>%
+    gsub(".*(\\d\\d)\\D.*", "\\1", .) %>%
+      .[nchar(.) == 2] %>%
+        sort()
+  
+  sortedFiles = sapply(years, grep, files, value = TRUE)
+  headerFix = lapply(sortedFiles, .ReadXls, n_max = 10) %>%
+    sapply(., function(x) { min(which(! is.na(x[, 2]))) })
+  
+  mapply(sortedFiles, headerFix, years, SIMPLIFY = FALSE, FUN = function(x, y, z) {
+    .ReadXls(x, skip = y) %>%
+      magrittr::set_colnames(., gsub("\\n$", "", colnames(.))) %>%
+        dplyr::mutate(FilenameYear = z)
+  }) %>%
+    do.call(rbind, .)
+}
+
